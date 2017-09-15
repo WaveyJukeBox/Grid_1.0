3079884f8301f14d978634d4db979d3a3b47851a
Christopher Norman <cjnorman8787@gmail.com>
2017-09-15 10:25:17 -0500

    Starting Grid off fresh

---
+++ Grid_1.0/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Grid_1.0
+//
+//  Created by Christopher Norman on 9/15/17.
+//  Copyright © 2017 yakuza. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
